2c3c992a0630d1bc2d29e8b9bc7904e86a4128a8
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-25 21:25:04 +0200

    add test to notification reducer

---
+++ src/store/reducer/notificationsReducer.test.ts
@@ -0,0 +1,30 @@
+import reducer, {
+  selectNotifications,
+  Notification,
+  Notifications,
+} from './notificationsReducer';
+
+describe('notifications reducer', () => {
+  it('should return initialState', () => {
+    const initialState: Notifications = [];
+
+    expect(reducer(undefined, { type: 'mockType' })).toStrictEqual(
+      initialState
+    );
+  });
+});
+
+describe('selectors', () => {
+  const exampleNotification: Notification = {
+    notification: 'This is a Notification',
+  };
+  it('should select notifications', () => {
+    const mockStore: Notifications = [exampleNotification];
+
+    const notifications = selectNotifications(mockStore);
+
+    expect(notifications).toStrictEqual([
+      { notification: 'This is a Notification' },
+    ]);
+  });
+});
