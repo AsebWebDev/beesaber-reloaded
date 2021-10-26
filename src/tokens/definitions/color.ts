ce2d0fb4033bcb6e5bab0c13f0a0188adfd55a81
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-26 11:33:15 +0200

    add colors

--- src/tokens/definitions/color.ts
+++ src/tokens/definitions/color.ts
@@ -1,14 +1,37 @@
-const page = {
+type ColorSet = {
+  light: string;
+  main: string;
+  shadow?: string;
+};
+
+type PageSet = {
+  bgColor: string;
+};
+
+const page: PageSet = {
   bgColor: '#00111f',
 };
 
-const red = {
+const blue: ColorSet = {
+  main: 'blue',
+  light: '#edf5ff',
+  shadow: '#7979ff',
+};
+
+const red: ColorSet = {
   main: '#ff0025',
   light: '#fff0f0',
   shadow: '#ff8d9e',
 };
 
+const yellow: ColorSet = {
+  main: '#ffeb00',
+  light: '#fffcdd',
+};
+
 export default {
+  blue,
   page,
   red,
+  yellow,
 };
