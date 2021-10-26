a29566f93cb68fbb94aab41aa2b0ea538a44b3f2
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-26 22:20:11 +0200

    add UserData shared type

---
+++ src/sharedTypes/UserData.ts
@@ -0,0 +1,11 @@
+export type UserData = {
+  countryRank: number;
+  county: string;
+  googleId: string;
+  password: string;
+  profilePic: string;
+  rank: number;
+  totalPlayCount: number;
+  totalScore: number;
+  username: string;
+};
--- src/store/reducer/userDataReducer.ts
+++ src/store/reducer/userDataReducer.ts
@@ -2,23 +2,13 @@ import { createAction, createSlice } from '@reduxjs/toolkit';
 
 import prefixActionType from '../helper/prefixActionType';
 
+import type { UserData } from '../../sharedTypes/UserData';
+
 const name = 'userData';
 
 const userDataActions = prefixActionType(name);
 
 const updateUserData = createAction(userDataActions('updateUserData'));
-
-type UserData = {
-  countryRank: number;
-  county: string;
-  googleId: string;
-  password: string;
-  profilePic: string;
-  rank: number;
-  totalPlayCount: number;
-  totalScore: number;
-  username: string;
-};
 
 const initialState: Record<string, unknown> | UserData = {};
 
@@ -45,6 +35,3 @@ export { selectUserData };
 
 // REDUCER EXPORT
 export default slice.reducer;
-
-// TYPES EXPORT
-export type { UserData };
