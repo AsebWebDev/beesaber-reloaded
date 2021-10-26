a38d73d1911ecae2e470fc459777ee0d4c08c757
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-26 11:33:47 +0200

    add brandlogo component

---
+++ src/components/common/BrandLogo/BrandLogo.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import tokens from 'src/tokens/index';
+import styled from 'styled-components';
+
+import { mediaQuery } from '../../../tokens/definitions/layout';
+import BrandLogoI from './BrandLogoI';
+
+const Container = styled.div`
+  position: relative;
+  height: 150px;
+  width: 150px;
+  font-size: 80px;
+
+  div {
+    position: absolute;
+    transform-origin: center;
+  }
+  div:first-child {
+    transform: rotate(90deg) translate(-36px, -68px);
+    z-index: 5;
+  }
+  div:nth-child(2) {
+    transform: rotate(150deg) translate(-103px, -49px);
+    z-index: 4;
+  }
+  div:nth-child(3) {
+    transform: rotate(30deg) translate(120px, -19px);
+  }
+  div:nth-child(4) {
+    transform: rotate(90deg) translate(70px, -68px);
+  }
+  div:nth-child(5) {
+    transform: rotate(150deg) translate(2px, -49px);
+  }
+  div:nth-child(6) {
+    transform: rotate(30deg) translate(15px, -20px);
+    z-index: 6;
+  }
+
+  .saber {
+    position: absolute;
+    height: 100%;
+    width: 3px;
+    top: 0%;
+    border-radius: 3px;
+  }
+
+  .saber.neon-red {
+    left: 49%;
+    transform: rotate(30deg);
+    background-color: ${tokens.color.red.light};
+    box-shadow: 0px 0px 8px 2px ${tokens.color.red.shadow},
+      0px 0px 12px 6px ${tokens.color.red.main};
+  }
+
+  .saber.neon-blue {
+    right: 49%;
+    transform: rotate(-30deg);
+    background-color: ${tokens.color.blue.light};
+    box-shadow: 0px 0px 8px 2px ${tokens.color.red.shadow},
+      0px 0px 12px 6px ${tokens.color.red.main};
+  }
+
+  ${mediaQuery.mobile} {
+    margin: 15px 0;
+  }
+
+  ${mediaQuery.sm} {
+    width: 50%;
+  }
+
+  ${mediaQuery.md} {
+    width: 30%;
+  }
+
+  ${mediaQuery.lg} {
+    width: 20%;
+  }
+
+  ${mediaQuery.xl} {
+    width: 10%;
+  }
+`;
+
+const BrandLogo = (): JSX.Element => (
+  <Container>
+    {/* neon honeycomb logo: */}
+    <BrandLogoI />
+    <BrandLogoI />
+    <BrandLogoI />
+    <BrandLogoI />
+    <BrandLogoI />
+    <BrandLogoI />
+  </Container>
+);
+
+export default BrandLogo;
---
+++ src/components/common/BrandLogo/BrandLogoI.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import tokens from '../../../tokens/index';
+
+const yellow = tokens.color.yellow;
+
+const Container = styled.div`
+  font-family: 'NeonTubes2';
+`;
+
+const IStroke = styled.span`
+  color: ${yellow.light};
+  text-shadow: ${yellow.main} -2px -1px 5px, ${yellow.main} 2px 1px 5px,
+    ${yellow.main} 0px 0px 10px, ${yellow.main} 0px 0px 15px,
+    ${yellow.main} 0px 0px 20px, ${yellow.main} 0px 0px 30px;
+`;
+
+const BrandLogoI = (): JSX.Element => (
+  <Container>
+    <IStroke>I</IStroke>
+  </Container>
+);
+
+export default BrandLogoI;
