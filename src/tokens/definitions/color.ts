9f676ef5055c5d1ee13968ad429a8a596b0fc406
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-26 14:35:47 +0200

    add createGlow function

--- src/tokens/definitions/color.ts
+++ src/tokens/definitions/color.ts
@@ -29,10 +29,25 @@ const yellow: ColorSet = {
   light: '#fffcdd',
 };
 
+const createGlow = (color: ColorSet): string => `
+  color: ${color.light};
+  -webkit-transition: color 0.6s ease-out, text-shadow 0.6s ease-out;
+  -moz-transition: color 0.6s ease-out, text-shadow 0.6s ease-out;
+  -o-transition: color 0.6s ease-out, text-shadow 0.6s ease-out;
+  transition: color 0.6s ease-out, text-shadow 0.6s ease-out;
+  text-shadow: ${color.main} -2px -1px 5px, ${color.main} 2px 1px 5px,
+    ${color.main} 0px 0px 10px, ${color.main} 0px 0px 15px, ${color.main} 0px 0px 20px,
+    ${color.main} 0px 0px 30px;
+`;
+const allColorSetsArray = [blue, red, yellow];
+
+export { allColorSetsArray };
+
 export type { ColorSet, PageSet };
 
 export default {
   blue,
+  createGlow,
   page,
   red,
   yellow,
