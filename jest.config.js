76db3ea6892435b2c19fb30a6f4e3c318564129c
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-27 12:26:45 +0200

    adjust jest setup configuratin

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
-/* eslint-disable strict */
 /* eslint-disable import/no-commonjs */
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testPathIgnorePatterns: [
+    "dist"
+  ],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    '^img/': '<rootDir>/src/testing/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/src/testing/__mocks__/styleMock.js',
+  },
 };
