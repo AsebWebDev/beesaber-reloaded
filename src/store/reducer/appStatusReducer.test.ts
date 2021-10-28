6e21340a08507a1643376bfd6f6055dd0db42e05
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-28 23:40:12 +0200

    add tests for reducer actions

--- src/store/reducer/appStatusReducer.test.ts
+++ src/store/reducer/appStatusReducer.test.ts
@@ -1,48 +1,83 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import reducer, {
   selectIsLoggedIn,
   selectIsLoggingIn,
+  userIsLoggedIn,
+  userIsLogginIn,
 } from './appStatusReducer';
 
 import type { RootState } from '../store';
 import type { AppStatus } from './appStatusReducer';
 
+const appStatus: AppStatus = {
+  isLoggedIn: false,
+  isLoggingIn: false,
+};
+
+const initialStore: RootState = {
+  appStatus,
+  notifications: [],
+  userData: {},
+};
+
 describe('appStatus reducer', () => {
   it('should return initialState', () => {
-    const initialState: AppStatus = {
-      isLoggedIn: false,
-      isLoggingIn: false,
-    };
-
-    expect(reducer(undefined, { type: 'mockType' })).toStrictEqual(
-      initialState
-    );
+    expect(reducer(undefined, { type: 'mockType' })).toStrictEqual(appStatus);
   });
 });
 
 describe('selector:', () => {
-  const exampleAppStatus: AppStatus = {
-    isLoggedIn: true,
-    isLoggingIn: false,
-  };
-  const mockStore: RootState = {
-    notifications: [],
-    appStatus: exampleAppStatus,
-    userData: {},
-  };
-
   describe('selectIsLoggedIn', () => {
     it('should select isLoggedIn', () => {
-      const appStatus = selectIsLoggedIn(mockStore);
+      const selectedAppStatus = selectIsLoggedIn(initialStore);
 
-      expect(appStatus).toStrictEqual(exampleAppStatus.isLoggedIn);
+      expect(selectedAppStatus).toStrictEqual(appStatus.isLoggedIn);
     });
   });
 
   describe('selectIsLoggingIn', () => {
     it('should select isLoggingIn', () => {
-      const appStatus = selectIsLoggingIn(mockStore);
+      const selectedAppStatus = selectIsLoggingIn(initialStore);
 
-      expect(appStatus).toStrictEqual(exampleAppStatus.isLoggingIn);
+      expect(selectedAppStatus).toStrictEqual(appStatus.isLoggingIn);
     });
   });
+});
+
+describe('actions', () => {
+  const middlewares = [thunk];
+
+  const mockStore = configureMockStore(middlewares)(initialStore);
+
+  beforeEach(() => mockStore.clearActions());
+
+  it.each([true, false])(
+    'should dispatch userIsLogginIn with %s',
+    (payload) => {
+      mockStore.dispatch(userIsLogginIn(payload));
+      const actions = mockStore.getActions();
+      const expectedPayload = {
+        type: 'appStatus/userIsLogginIn',
+        payload,
+      };
+
+      expect(actions).toStrictEqual([expectedPayload]);
+    }
+  );
+
+  it.each([true, false])(
+    'should dispatch userIsLoggedIn with %s',
+    (payload) => {
+      mockStore.dispatch(userIsLoggedIn(payload));
+      const actions = mockStore.getActions();
+      const expectedPayload = {
+        type: 'appStatus/userIsLoggedIn',
+        payload,
+      };
+
+      expect(actions).toStrictEqual([expectedPayload]);
+    }
+  );
 });
--- src/store/reducer/userDataReducer.test.ts
+++ src/store/reducer/userDataReducer.test.ts
@@ -1,6 +1,38 @@
-import reducer, { selectUserData } from './userDataReducer';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
+import reducer, { selectUserData, userDataUpdated } from './userDataReducer';
+
+import type { AppStatus } from 'src/store/reducer/appStatusReducer';
+import type { RootState } from 'src/store/store';
 import type { UserData } from '../../sharedTypes/UserData';
+
+const middlewares = [thunk];
+
+const appStatus: AppStatus = {
+  isLoggedIn: true,
+  isLoggingIn: false,
+};
+
+const initialStore: RootState = {
+  appStatus,
+  notifications: [],
+  userData: {},
+};
+
+const exampleUserData: UserData = {
+  countryRank: 111,
+  county: 'Sweden',
+  googleId: '123',
+  password: 'myPassword',
+  profilePic: 'url',
+  rank: 42,
+  totalPlayCount: 42,
+  totalScore: 1312,
+  username: 'lovenotwar',
+};
+
+const mockStore = configureMockStore(middlewares)(initialStore);
 
 describe('userData reducer', () => {
   it('should return initialState', () => {
@@ -13,23 +45,22 @@ describe('userData reducer', () => {
 });
 
 describe('selectors', () => {
-  const exampleUserData: UserData = {
-    countryRank: 111,
-    county: 'Sweden',
-    googleId: '123',
-    password: 'myPassword',
-    profilePic: 'url',
-    rank: 42,
-    totalPlayCount: 42,
-    totalScore: 1312,
-    username: 'lovenotwar',
-  };
-
   it('should select notifications', () => {
-    const mockStore: UserData = exampleUserData;
-
-    const userData = selectUserData(mockStore);
+    const userData = selectUserData(exampleUserData);
 
     expect(userData).toStrictEqual(exampleUserData);
+  });
+});
+
+describe('actions', () => {
+  it('should dispatch userDataUpdated', () => {
+    mockStore.dispatch(userDataUpdated(exampleUserData));
+    const actions = mockStore.getActions();
+    const expectedPayload = {
+      type: 'userData/userDataUpdated',
+      payload: exampleUserData,
+    };
+
+    expect(actions).toStrictEqual([expectedPayload]);
   });
 });
