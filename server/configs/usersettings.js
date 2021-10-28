58f2f05b1accf9e1d864e7281b983d48b898ada6
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-28 17:28:57 +0200

    remove unused Effects settings

--- server/configs/usersettings.js
+++ server/configs/usersettings.js
@@ -15,14 +15,6 @@ const userSettings = {
         intervalUpdatecheck: {
           val: 30000, name: "Frequency: Check for Update" // Default: 30 Sekunden 
         },
-      },
-      Effects: {
-        parallax: {
-          val: true, name: "Parallax"
-        },
-        slideGallery: {
-          val: true, name: "Slide Gallery"
-        }
       }
     }
 }
