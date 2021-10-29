6d985b44e37e183b45db9fdd59d1ab3698300689
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-29 22:59:16 +0200

    add tests to auth api

---
+++ src/api/authApi.test.ts
@@ -0,0 +1,71 @@
+import api from './api';
+
+import type { UserData } from 'src/sharedTypes/UserData';
+import type { GoogleResponse } from './authApi';
+
+jest.mock('axios');
+
+const exampleUserData: UserData = {
+  countryRank: 111,
+  county: 'Sweden',
+  googleId: '123',
+  password: 'myPassword',
+  profilePic: 'url',
+  rank: 42,
+  totalPlayCount: 42,
+  totalScore: 1312,
+  username: 'lovenotwar',
+};
+
+const googleResponse: GoogleResponse = {
+  googleId: '123',
+  profileObj: {
+    imageUrl: 'url',
+    name: 'Name',
+  },
+};
+
+describe('authApi', () => {
+  describe('googleLogin', () => {
+    let spy: jest.Mock | jest.SpyInstance<Promise<UserData>, [GoogleResponse]>;
+
+    beforeEach(() => {
+      spy = jest.fn();
+      spy = jest.spyOn(api.authApi, 'googleLogin');
+    });
+
+    afterEach(() => spy.mockRestore());
+
+    it.todo('saved UserData to sessionStorage');
+
+    it('return userData when called', async () => {
+      spy.mockResolvedValue(exampleUserData);
+      const response = await api.authApi.googleLogin(googleResponse);
+
+      expect(response).toStrictEqual(exampleUserData);
+    });
+
+    it('return empty object when call failed', async () => {
+      const response = await api.authApi.googleLogin(googleResponse);
+
+      expect(response).toStrictEqual({});
+    });
+  });
+
+  describe('isValidMongoId', () => {
+    const spy = jest.spyOn(api.authApi, 'isValidMongoId');
+    const childFunction = jest.fn(api.authApi.isValidMongoId.bind(this));
+    const isValidMongoId = async (id: string) => childFunction(id);
+
+    spy.mockResolvedValue(true);
+
+    it('should call isValidMongoId fom auth api', async () => {
+      await isValidMongoId('123');
+      expect(childFunction).toHaveBeenCalledTimes(1);
+      expect(childFunction).toHaveBeenCalledWith('123');
+      expect(spy).toBeCalledTimes(1);
+    });
+  });
+
+  it.todo('logout');
+});
