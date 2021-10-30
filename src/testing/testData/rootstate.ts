f40b1ee328f2a7c07bf42c8e0c06c79370d41791
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-30 23:34:28 +0200

    add rootState for testing

---
+++ src/testing/testData/rootstate.ts
@@ -0,0 +1,15 @@
+import type { AppStatus } from '../../store/reducer/appStatusReducer';
+import type { RootState } from '../../store/store';
+
+const appStatus: AppStatus = {
+  isLoggedIn: true,
+  isLoggingIn: false,
+};
+
+const store: RootState = {
+  appStatus,
+  notifications: [],
+  userData: {},
+};
+
+export default store;
