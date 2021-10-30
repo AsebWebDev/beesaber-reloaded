678e956a61229bc2652e4f20b420afa6bbc7e0e3
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-30 23:35:25 +0200

    add tests for landing page

---
+++ src/components/pages/LandingPage.test.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import rootState from '../../testing/testData/rootstate';
+import LandingPage from './LandingPage';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('LandingPage', () => {
+  it.each([false, true] as const)(
+    'should match the snapshot when isLoggedIn is %s',
+    (isLoggingIn) => {
+      rootState.appStatus.isLoggingIn = isLoggingIn;
+      const { container } = render(
+        <Provider store={mockStore(rootState)}>
+          <LandingPage />
+        </Provider>
+      );
+
+      expect(container.firstChild).toMatchSnapshot();
+    }
+  );
+});
