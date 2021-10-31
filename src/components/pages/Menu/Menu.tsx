997d43cedac0dc71d89af1afd72564fdeeebfc44
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-10-31 09:25:45 +0100

    add basic menu content component

---
+++ src/components/pages/Menu/Menu.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+// import { Link } from 'react-router-dom';
+import { selectIsLoggedIn } from '../../../store/reducer/appStatusReducer';
+import BrandLogo from '../../common/BrandLogo/BrandLogo';
+import GlowingText from '../../common/GlowingText/GlowingText';
+import GoogleOAuth from '../../common/GoogleOAuth/GoogleOAuth';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'NeonTubes2';
+  padding: 25px 20px;
+`;
+
+const Menu = (): JSX.Element => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  // eslint-disable-next-line no-console
+  console.log('🚀 ~ file: Menu.tsx ~ line 12 ~ isLoggedIn', isLoggedIn);
+
+  return (
+    <Container>
+      <BrandLogo />
+      <div id="menu-main">
+        <div id="menu-head">
+          {/* <GoogleProfileData /> */}
+          <GlowingText as="h1" titleColor="red">
+            Bee
+          </GlowingText>
+          <br></br>
+          <GlowingText as="h1" titleColor="blue">
+            Saber
+          </GlowingText>
+          <GoogleOAuth />
+        </div>
+        {/* {isLoggedIn && (
+          <div id="menu-points">
+            <Link to="/">
+              <span className="neon-red">Dashboard</span>
+            </Link>
+            <Link to="/myprofile">
+              <span className="neon-red">My</span>{' '}
+              <span className="neon-blue">Profile</span>
+            </Link>
+            <Link to="/myhive">
+              <span className="neon-red">My</span>{' '}
+              <span className="neon-blue">Hive</span>
+            </Link>
+          </div>
+        )} */}
+      </div>
+
+      {/* alternative logo made with svg: */}
+      {/* <svg className="logo"><use xlinkHref="#logo-honeycomb" /></svg> */}
+    </Container>
+  );
+};
+
+export default Menu;
