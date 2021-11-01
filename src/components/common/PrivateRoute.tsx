fe490fd00c061f89020fd6f440a8b3b7e88b8bd1
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-01 20:58:58 +0100

    add private route component

---
+++ src/components/common/PrivateRoute.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, useHistory } from 'react-router-dom';
+
+import api from '../../api/api';
+
+// import { useAppSelector } from '../../store/hooks';
+// import { selectIsLoggedIn } from '../../store/reducer/appStatusReducer';
+import type { ComponentType } from 'react';
+import type { StaticContext } from 'react-router';
+import type { RouteComponentProps } from 'react-router-dom';
+
+type Props = {
+  component:
+    | ComponentType<
+        RouteComponentProps<
+          Record<string, string | undefined>,
+          StaticContext,
+          unknown
+        >
+      >
+    | ComponentType<JSX.Element>;
+  path: string;
+};
+
+function PrivateRoute({ component, path }: Props): JSX.Element {
+  //   const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const isLoggedIn = api.authApi.isLoggedIn();
+  const history = useHistory();
+
+  if (!isLoggedIn) history.push('/');
+
+  return <Route path={path} component={component} />;
+}
+
+export default PrivateRoute;
