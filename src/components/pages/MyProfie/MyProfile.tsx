27cbaae3af093106e8d888a6e97aa2ce92503f78
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-02 22:55:38 +0100

    add MyProfile component

--- src/components/pages/MyProfie/MyProfile.tsx
+++ src/components/pages/MyProfie/MyProfile.tsx
@@ -1,3 +1,54 @@
-const MyProfile = (): JSX.Element => <div>MyProfile</div>;
+/* eslint-disable no-console */
+import { MDBBtn, MDBIcon, MDBInput } from 'mdb-react-ui-kit';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import Title from '@/components/common/Title/Title';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IdForm = styled.form`
+  display: flex;
+  align-items: center;
+`;
+
+const MyProfile = (): JSX.Element => {
+  const [myScoreSaberId, setMyScoreSaberId] = useState<string>('123');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setMyScoreSaberId(e.target.value);
+
+  const handleSave = () => {
+    console.log('Handle Save');
+  };
+
+  return (
+    <Container>
+      <Title as="h1">My Profile</Title>
+      <IdForm>
+        <MDBInput
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
+          value={myScoreSaberId}
+          label="Your ScoreSaber ID"
+          icon="user"
+          group="true"
+          type="text"
+          validate="true"
+          error="wrong"
+          success="right"
+        />
+        <MDBBtn onClick={handleSave} size="sm" outline color="secondary">
+          Save
+          <MDBIcon far icon="paper-plane" className="ml-1" />
+        </MDBBtn>
+      </IdForm>
+    </Container>
+  );
+};
 
 export default MyProfile;
