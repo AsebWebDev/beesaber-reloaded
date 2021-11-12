ea404cf263a6e093417ab19cd19d51a90f3b04e1
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-12 23:53:03 +0100

    use try/catch and proper typing

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,24 +1,73 @@
+import {
+  parseFullPlayerQueryUrl,
+  parseGetRecentScoresUrl,
+} from '@/helper/urlParser';
+
 import { errHandler, service } from './api';
 
-import type { UserData } from '@/sharedTypes/UserData';
+import type { AxiosResponse } from 'axios';
+import type { ScoreSaberUserInfo, UserData, UserScores } from '@/sharedTypes';
 import type { PossibleResponses } from './api';
 
+const ErrorCodes = [404, 429, 422];
+
+type ApiCallProps = {
+  method?: 'get' | 'post';
+  payload?: Partial<UserData>;
+  url: string;
+};
+
+type UserApiResponse = ScoreSaberUserInfo | UserData | UserScores;
+
+const doApiCall = async ({
+  url,
+  method = 'get',
+  payload,
+}: ApiCallProps): Promise<UserApiResponse> => {
+  try {
+    const response: AxiosResponse<UserApiResponse> = await service[method](
+      url,
+      payload
+    );
+
+    if (ErrorCodes.includes(response.status)) throw new Error();
+
+    return response.data;
+  } catch (err: unknown) {
+    errHandler(err as PossibleResponses);
+    throw new Error();
+  }
+};
+
 const userApi = {
-  async getUserData(userId: string): Promise<unknown> {
-    return service
-      .get<UserData>('/user/' + userId)
-      .then(({ data }) => data)
-      .catch((err: unknown) => errHandler(err as PossibleResponses));
+  async getUserScores(id: string): Promise<UserScores> {
+    const url = parseGetRecentScoresUrl(id);
+    const recentScores = await doApiCall({ url });
+
+    return recentScores as UserScores;
+  },
+  async getSSUserInfo(id: string): Promise<ScoreSaberUserInfo> {
+    const url = parseFullPlayerQueryUrl(id);
+    const result = await doApiCall({ url });
+
+    return result as ScoreSaberUserInfo;
+  },
+
+  async getUserData(userId: string): Promise<UserData> {
+    const url = `/user/${userId}`;
+    const result = await doApiCall({ url });
+
+    return result as UserData;
   },
 
   async saveUserData(
     userId: string,
     userdata: Partial<UserData>
   ): Promise<UserData | string> {
-    return service
-      .post<UserData>('/user/' + userId, userdata)
-      .then<UserData>(({ data }) => data)
-      .catch((err: unknown) => errHandler(err as PossibleResponses));
+    const url = `/user/${userId}`;
+    const result = await doApiCall({ url, method: 'post', payload: userdata });
+
+    return result as UserData;
   },
 };
 
