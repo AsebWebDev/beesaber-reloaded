08226bf96e323a4a13e522f87c6242334e273c5b
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-13 09:39:33 +0100

    add index for shared types

---
+++ src/sharedTypes/index.ts
@@ -0,0 +1,14 @@
+import type ScoreSaberUserInfo from './ScoreSaberUserInfo';
+import type Settings from './Settings';
+import type StyledCSSValue from './StyledCSSValue';
+import type { UserData } from './UserData';
+import type { ScoreData, UserScores } from './UserScores';
+
+export type {
+  ScoreData,
+  ScoreSaberUserInfo,
+  Settings,
+  StyledCSSValue,
+  UserData,
+  UserScores,
+};
