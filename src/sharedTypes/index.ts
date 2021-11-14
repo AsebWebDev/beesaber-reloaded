5d076ee7da0c3de6da855766e1ee720471d1aa37
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-14 19:37:33 +0100

    export Score / Scores Types

--- src/sharedTypes/index.ts
+++ src/sharedTypes/index.ts
@@ -2,10 +2,12 @@ import type ScoreSaberUserInfo from './ScoreSaberUserInfo';
 import type Settings from './Settings';
 import type StyledCSSValue from './StyledCSSValue';
 import type { UserData } from './UserData';
-import type { ScoreData, UserScores } from './UserScores';
+import type { Score, ScoreData, Scores, UserScores } from './UserScores';
 
 export type {
+  Score,
   ScoreData,
+  Scores,
   ScoreSaberUserInfo,
   Settings,
   StyledCSSValue,
