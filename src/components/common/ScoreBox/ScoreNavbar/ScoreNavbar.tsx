de6591868060f65fbb5ccfc479df219f3cfa6cd2
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-21 20:23:28 +0100

    fixup! move ScoreNavBar to own component

--- src/components/common/ScoreBox/ScoreNavbar/ScoreNavbar.tsx
+++ src/components/common/ScoreBox/ScoreNavbar/ScoreNavbar.tsx
@@ -4,23 +4,42 @@ import {
   MDBNavbarLink,
   MDBSwitch,
 } from 'mdb-react-ui-kit';
-import React from 'react';
+import styled from 'styled-components';
+
+import tokens from '@/tokens';
+
+const NavBar = styled(MDBNavbar)`
+  background-color: ${tokens.color.page.bgColor.light};
+  list-style: none;
+  padding: 0 1rem;
+
+  .nav-link {
+    color: ${tokens.color.white.light};
+  }
+
+  .active {
+    background-color: ${tokens.color.white.light};
+    color: ${tokens.color.page.bgColor.main};
+  }
+`;
+
+const NavigationItems = styled.div`
+  display: flex;
+`;
 
 type Props = {
   activeitem: string;
   setIsPlayedByHive: (isOn: boolean) => void;
   toggleTab: (id: string) => void;
 };
+
 const ScoreNavbar = ({
   activeitem,
   setIsPlayedByHive,
   toggleTab,
-}: Props): JSX.Element => {
-  // eslint-disable-next-line no-console
-  console.log('🚀 ~ file: ScoreNavbar.tsx ~ line 10 ~ activeitem', activeitem);
-
-  return (
-    <MDBNavbar className="nav-tabs mt-4 ml-2 mr-2">
+}: Props): JSX.Element => (
+  <NavBar>
+    <NavigationItems>
       <MDBNavbarItem>
         <MDBNavbarLink
           to="#"
@@ -41,14 +60,14 @@ const ScoreNavbar = ({
           Top
         </MDBNavbarLink>
       </MDBNavbarItem>
-      <MDBNavbarItem>
-        <MDBSwitch
-          label="songs only shared by hive "
-          onChange={(isOn: boolean) => setIsPlayedByHive(isOn)}
-        />
-      </MDBNavbarItem>
-    </MDBNavbar>
-  );
-};
+    </NavigationItems>
+    <MDBNavbarItem>
+      <MDBSwitch
+        label="songs only shared by hive "
+        onChange={(isOn: boolean) => setIsPlayedByHive(isOn)}
+      />
+    </MDBNavbarItem>
+  </NavBar>
+);
 
 export default ScoreNavbar;
