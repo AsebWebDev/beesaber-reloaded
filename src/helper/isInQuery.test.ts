fa6693d5119874759d4479d9ad95f90289f744bb
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-21 17:52:05 +0100

    add tests for isInQuery helper

---
+++ src/helper/isInQuery.test.ts
@@ -0,0 +1,25 @@
+import exampleUserScores from '@/testing/testData/exampleUserScores';
+
+import isInQuery from './isInQuery';
+
+const score = exampleUserScores.scores[0];
+
+type Props = {
+  query: string;
+  result: boolean;
+};
+
+describe('isInQuery', () => {
+  it.each`
+    name              | authorName              | query      | result
+    ${score.songName} | ${score.songAuthorName} | ${'begun'} | ${true}
+    ${score.songName} | ${score.songAuthorName} | ${'star'}  | ${true}
+    ${score.songName} | ${score.songAuthorName} | ${'Vader'} | ${false}
+    ${score.songName} | ${score.songAuthorName} | ${'   '}   | ${false}
+  `(
+    'should return $result when query is "$query" and song is "$name" by "$authorName"',
+    ({ query, result }: Props) => {
+      expect(isInQuery(score, query)).toBe(result);
+    }
+  );
+});
--- src/helper/isInQuery.ts
+++ src/helper/isInQuery.ts
@@ -1,6 +1,9 @@
 import type { Score } from '@/sharedTypes';
 
 const isInQuery = (score: Score, query: string): boolean => {
+  // check for empty query
+  if (query.replace(/\s/g, '').length === 0) return false;
+
   const { songName, songAuthorName } = score;
 
   return (
