bbad617dcf837762a1444722261ea0994c899166
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-19 22:44:32 +0100

    add tests to DiffTag

---
+++ src/components/common/DiffTag/DiffTag.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+
+import DiffTag from './DiffTag';
+
+import type { Difficulty } from '@/sharedTypes/UserScores';
+
+describe('components/common/DiffTag', () => {
+  it.each([1, 3, 5, 7, 9] as Difficulty[])(
+    'should match snapshot when diffiultiy is %s',
+    (difficulty) => {
+      const { container } = render(<DiffTag difficulty={difficulty} />);
+
+      expect(container.firstChild).toMatchSnapshot();
+    }
+  );
+});
