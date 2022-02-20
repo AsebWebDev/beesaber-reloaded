878f8e97a9e2824a11c0584dd6bde16d27cf2ce8
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-20 18:26:39 +0100

    add tests to ScoreContent component

---
+++ src/components/common/ScoreBox/ScoreContent/ScoreContent.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+
+import exampleScores from '@/testing/testData/exampleScores';
+
+import ScoreContent from './ScoreContent';
+
+describe('components/common/ScoreContent/', () => {
+  it('should match snapshot', () => {
+    const { container } = render(<ScoreContent scores={exampleScores} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
