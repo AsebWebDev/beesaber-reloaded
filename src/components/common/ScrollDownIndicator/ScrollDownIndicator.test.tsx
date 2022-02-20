720939e468bd9d8c3c0979ec81036ac06562d263
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-20 19:18:16 +0100

    add test for null, when scrolling down

--- src/components/common/ScrollDownIndicator/ScrollDownIndicator.test.tsx
+++ src/components/common/ScrollDownIndicator/ScrollDownIndicator.test.tsx
@@ -1,11 +1,27 @@
 import { render } from '@testing-library/react';
 
+import * as useIsScrollingDown from '@/sharedHooks/useIsScrollingDown';
+
 import ScrollDownIndicator from './ScrollDownIndicator';
 
 describe('ScrollDownIndicator', () => {
+  const spy = jest.spyOn(useIsScrollingDown, 'default');
+
+  afterAll(() => {
+    spy.mockReset();
+  });
+
   it('should match the snapshot', () => {
     const { container } = render(<ScrollDownIndicator />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should return null when scrollig down', () => {
+    spy.mockReturnValue({ isScrollingDown: true });
+
+    const { container } = render(<ScrollDownIndicator />);
+
+    expect(container.firstChild).toBeNull();
   });
 });
