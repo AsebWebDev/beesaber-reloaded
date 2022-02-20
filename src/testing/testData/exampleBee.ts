ff5f941400dffb08227eaed51b7f20d7d08ce55f
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-20 19:13:07 +0100

    add example Bee

---
+++ src/testing/testData/exampleBee.ts
@@ -0,0 +1,11 @@
+import { exampleScoreData } from '@/testing/testData/exampleScores';
+
+import type { Bee } from '@/sharedTypes/Bee';
+
+const exampleBee: Bee = {
+  playerId: '34365656',
+  playerName: 'example Bee',
+  scoreData: exampleScoreData,
+};
+
+export default exampleBee;
