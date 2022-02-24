5daff10591e51dbcc6c3998e05ff6db4e6a1572e
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-24 17:41:56 +0100

    update apiPlayer handlers

--- src/api/services/apiPlayer/apiPlayerHandler.ts
+++ src/api/services/apiPlayer/apiPlayerHandler.ts
@@ -1,8 +1,27 @@
 import { rest } from 'msw';
 
+import exampleScores from '@/testing/testData/exampleScores';
+import exampleSSUserInfo from '@/testing/testData/exampleSSUserInfo';
 import exampleUserData from '@/testing/testData/exampleUserData';
 
 const handlers = [
+  // getRecentScoresUrl
+  rest.get(
+    'https://new.scoresaber.com/api/player/:id/scores/recent/:count',
+    (req, res, ctx) => res(ctx.status(200), ctx.json(exampleScores))
+  ),
+
+  // useGetPlayersByNameQuery
+  rest.get(
+    'https://new.scoresaber.com/api/players/by-name/:name',
+    (req, res, ctx) => {
+      console.log('name: ', req.params.name);
+
+      return res(ctx.status(200), ctx.json([exampleSSUserInfo.playerInfo]));
+    }
+  ),
+
+  // useGetFullPlayerQuery
   rest.get(
     'https://new.scoresaber.com/api/player/:id/full',
     (req, res, ctx) => {
