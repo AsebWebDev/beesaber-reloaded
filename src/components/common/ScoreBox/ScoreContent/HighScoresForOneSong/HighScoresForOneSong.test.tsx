ee28b041b3f10a7fc05a622e16da2cccd105d350
André Sebastian-Bath <a.sebastian@posteo.de>
2022-02-28 22:29:03 +0100

    use faketimers for tests

--- src/components/common/ScoreBox/ScoreContent/HighScoresForOneSong/HighScoresForOneSong.test.tsx
+++ src/components/common/ScoreBox/ScoreContent/HighScoresForOneSong/HighScoresForOneSong.test.tsx
@@ -10,6 +10,15 @@ describe('components/common/ScoreBox/ScoreContent/HighScoresForOneSong', () => {
   const highscore = exampleScores[0];
   const highscorePlayedByHive = exampleScores[2];
 
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date('2021-12-01').getTime());
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it.each`
     score                    | description
     ${highscore}             | ${'not played by hive'}
