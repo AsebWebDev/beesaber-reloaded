2592f31b44dd0ff6770aa15cb662028ebef055da
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-02 21:46:05 +0100

    add shared GooglePayLoad type

---
+++ src/sharedTypes/GooglePayload.ts
@@ -0,0 +1,13 @@
+type GooglePayload = {
+  googleId: string;
+  profileObj: {
+    email: string;
+    familyName: string;
+    givenName: string;
+    googleId: string;
+    imageUrl: string;
+    name: string;
+  };
+};
+
+export default GooglePayload;
--- src/sharedTypes/index.ts
+++ src/sharedTypes/index.ts
@@ -1,4 +1,5 @@
 import type { Bee } from './Bee';
+import type GooglePayload from './GooglePayload';
 import type ScoreSaberUserInfo from './ScoreSaberUserInfo';
 import type Settings from './Settings';
 import type StyledCSSValue from './StyledCSSValue';
@@ -7,6 +8,7 @@ import type { Score, ScoreData, Scores, UserScores } from './UserScores';
 
 export type {
   Bee,
+  GooglePayload,
   GoogleUserData,
   Score,
   ScoreData,
