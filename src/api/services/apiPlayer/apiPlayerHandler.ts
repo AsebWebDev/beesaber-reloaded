d2c14a249c004f7950c93b39cda4f49fa5766089
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-05 10:32:05 +0100

    remove console.logs

--- src/api/services/apiPlayer/apiPlayerHandler.ts
+++ src/api/services/apiPlayer/apiPlayerHandler.ts
@@ -2,10 +2,9 @@ import { rest } from 'msw';
 
 import exampleScores from '@/testing/testData/exampleScores';
 import exampleSSUserInfo from '@/testing/testData/exampleSSUserInfo';
-import exampleUserData from '@/testing/testData/exampleUserData';
 
 const handlers = [
-  // getRecentScoresUrl
+  // getRecentScores
   rest.get(
     'https://new.scoresaber.com/api/player/:id/scores/recent/:count',
     (req, res, ctx) => res(ctx.status(200), ctx.json(exampleScores))
@@ -14,21 +13,13 @@ const handlers = [
   // useGetPlayersByNameQuery
   rest.get(
     'https://new.scoresaber.com/api/players/by-name/:name',
-    (req, res, ctx) => {
-      console.log('name: ', req.params.name);
-
-      return res(ctx.status(200), ctx.json([exampleSSUserInfo.playerInfo]));
-    }
+    (req, res, ctx) =>
+      res(ctx.status(200), ctx.json([exampleSSUserInfo.playerInfo]))
   ),
 
   // useGetFullPlayerQuery
-  rest.get(
-    'https://new.scoresaber.com/api/player/:id/full',
-    (req, res, ctx) => {
-      console.log('handle scoresabe: ', req.params.id);
-
-      return res(ctx.status(200), ctx.json(exampleSSUserInfo));
-    }
+  rest.get('https://new.scoresaber.com/api/player/:id/full', (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(exampleSSUserInfo))
   ),
 ];
 
