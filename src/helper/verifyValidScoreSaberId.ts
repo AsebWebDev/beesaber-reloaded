4fa323fbd9500fc19d83ca36796dd9b4b040fac9
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-05 10:37:05 +0100

    use RTKQ for verifyValidScoreSaberId

--- src/helper/verifyValidScoreSaberId.ts
+++ src/helper/verifyValidScoreSaberId.ts
@@ -1,14 +1,12 @@
-import api from '../api/api';
+import apiPlayer from '@/api/services/apiPlayer/apiPlayer';
+import store from '@/store/store';
 
-// TODO: Use RTKQ endpoint isValidMongoId
-const verifyValidScoreSaberId = async (id: string): Promise<void> => {
-  try {
-    await api.userApi.getRecentUserScores(id);
-  } catch (err: unknown) {
-    throw new Error(
-      'ScoreSaber Id does not exist. Please check your id again...'
-    );
-  }
+const verifyValidScoreSaberId = async (id: string): Promise<boolean> => {
+  const result = await store.dispatch(
+    apiPlayer.endpoints.getRecentScores.initiate({ id, count: 1 })
+  );
+
+  return result.error !== undefined && result.data !== undefined;
 };
 
 export default verifyValidScoreSaberId;
