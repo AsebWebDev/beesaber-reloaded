aedd1869c5461e4b14c68f327aeb7d5d839e3e53
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-17 21:14:55 +0100

    add getAllScores helper

---
+++ server/routes/helper/getAllScores.ts
@@ -0,0 +1,23 @@
+import { ScoreData } from '../../../sharedTypes';
+import { calcScoreHashed, calcTopScores } from './calcScores';
+import getRecentScores from './getRecentScores';
+
+const baseUrl = 'https://new.scoresaber.com/api';
+
+/**
+ * Fetch all Scores of a ScoreSaberUser
+ * @param {String} id - ScoreSaber id
+ * @returns {ScoreData} ScoreData with recentScores, topScores and all score hashes
+ */
+const getAllScores = async (id: string): Promise<ScoreData> => {
+  const threshold = 10;
+  const scoresRecent = await getRecentScores({ id, threshold });
+
+  return {
+    scoredSongsHashes: calcScoreHashed(scoresRecent),
+    scoresRecent,
+    scoresTop: calcTopScores(scoresRecent),
+  };
+};
+
+export default getAllScores;
