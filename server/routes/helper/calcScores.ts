5fa5e66af200c75a90ba782f20ba05728b4b5696
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-18 16:55:45 +0100

    add helper function to calculate all scores

--- server/routes/helper/calcScores.ts
+++ server/routes/helper/calcScores.ts
@@ -1,8 +1,18 @@
-import type { Scores } from '@/../../sharedTypes';
+import type { ScoreData, Scores } from '@/../../sharedTypes';
 
+/**
+ *
+ * @param scoresRecent
+ * @returns an array of scores sorted descending by score
+ */
 const calcTopScores = (scoresRecent: Scores): Scores =>
   [...scoresRecent].sort((a, b) => b.score - a.score);
 
+/**
+ *
+ * @param scoresRecent
+ * @returns and array of all score hashes
+ */
 const calcScoreHashed = (scoresRecent: Scores): string[] => {
   const scoredSongsHashes: string[] = [];
 
@@ -11,4 +21,19 @@ const calcScoreHashed = (scoresRecent: Scores): string[] => {
   return scoredSongsHashes;
 };
 
+/**
+ *
+ * @param scoresRecent
+ * @returns ScoreData including recent scores, top scores and all score hashes
+ */
+const calcAllScores = (scoresRecent: Scores): ScoreData => {
+  return {
+    scoresRecent,
+    scoredSongsHashes: calcScoreHashed(scoresRecent),
+    scoresTop: calcTopScores(scoresRecent),
+  };
+};
+
 export { calcScoreHashed, calcTopScores };
+
+export default calcAllScores;
