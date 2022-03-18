5920adbe0f68dd6ff537b82a7c47f1636f2e8a04
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-18 20:07:13 +0100

    add isUpdatedNeeded helper

---
+++ server/helper/IsUpdateNeeded.ts
@@ -0,0 +1,20 @@
+import { ScoreSaberUserInfo, UserData } from '../../sharedTypes';
+import User from '../models/User';
+import getPlayerById from '../routes/helper/getPlayerById';
+
+const isUpdateNeeded = async (userData: UserData): Promise<boolean> => {
+  const { _id: mongoId, myScoreSaberId } = userData;
+
+  const result: ScoreSaberUserInfo = await getPlayerById(myScoreSaberId);
+  const remotePlayCount = result.scoreStats.totalPlayCount;
+  const localPlayCount = await User.findById(mongoId)
+    .then(async (userDoc: UserData) => {
+      if (!userDoc) return;
+      return userDoc.totalPlayCount;
+    })
+    .catch((err: unknown) => console.log(err));
+
+  return remotePlayCount > localPlayCount;
+};
+
+export default isUpdateNeeded;
