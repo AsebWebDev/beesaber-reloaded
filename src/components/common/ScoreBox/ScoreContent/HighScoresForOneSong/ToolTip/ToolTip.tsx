7f23e6f1b54e66f77eb112f3474a320b43880f77
André Sebastian-Bath <a.sebastian@posteo.de>
2022-03-26 18:57:38 +0100

    add basic tooltip

--- src/components/common/ScoreBox/ScoreContent/HighScoresForOneSong/ToolTip/ToolTip.tsx
+++ src/components/common/ScoreBox/ScoreContent/HighScoresForOneSong/ToolTip/ToolTip.tsx
@@ -4,26 +4,21 @@ type Props = {
   playedBy?: PlayedBy[];
 };
 
-const ToolTip = ({ playedBy }: Props) => {
-  console.log('🚀 ~ file: ToolTip.tsx ~ line 8 ~ playedBy', playedBy);
+const ToolTip = ({ playedBy }: Props) => (
+  <ul>
+    {playedBy?.map((bee) => {
+      const { difficulty, playerId, playerName, myScore, beeScore } = bee;
 
-  return (
-    <ul>
-      {playedBy?.map((bee) => {
-        const { playerId, playerName, myScore, beeScore } = bee;
-        const difficulty = bee.difficulty.toString();
-
-        return (
-          <li key={`${playerId}-${difficulty}`}>
-            <p>{playerName}</p>
-            <p>{myScore}</p>
-            <p>{beeScore}</p>
-            <p>{difficulty}</p>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+      return (
+        <li key={`${playerId}-${difficulty}`}>
+          <p>{playerName}</p>
+          <p>{myScore}</p>
+          <p>{beeScore}</p>
+          <p>{difficulty}</p>
+        </li>
+      );
+    })}
+  </ul>
+);
 
 export default ToolTip;
